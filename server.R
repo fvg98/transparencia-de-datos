b0e8edc18c15725cc78d8085d0dd9e2a8a06c9cd
JMARTINEHE <jmart307@itam.mx>
2021-05-15 07:56:45 -0500

    Modifico front y página de publicaciones

--- server.R
+++ server.R
@@ -330,10 +330,10 @@ server <- function(session, input, output) {
   #Texto a mostrar en landing pages
   output$header_1 <- renderText({paste("<center><b>Datalab</b></center>" ,"<center>Transparencia legislativa</center>",sep=" ")})
   output$header_2 <- renderText({paste("<center><b>Nuestro objetivo es facilitar el acceso a información legislativa</b></center>")})
-  output$header_3 <- renderText({paste("<center><b>Publicaciones</b></center>","<center><h4>Haz clic en el Tweet para ver el hilo</h4></center>",sep = " ")})
+  output$header_3 <- renderText({paste("<center><b>Publicaciones</b></center>","<center><h3>Aquí puedes ver el resto de nuestras visualizaciones</h3></center>","<center><h4>Haz clic en el Tweet para ver el hilo</h4></center>",sep = " ")})
   output$header_4 <- renderText({paste("<center><b>(Recurso del INE)</b></center>","<center><h4>Te dejamos un pequeño tutorial de como utilizarlo:</h4></center>",sep = " ")})
   output$header_5 <- renderText({paste("<center><b>¡Te damos la bienvenida!</b></center>","<center><h4>Somos un grupo estudiantil que busca aportar en este proceso electoral</h4></center>",sep = " ")})
-  output$header_6 <- renderText({paste("<h4><b>Te dejamos un video para guiarte por la página:</b></h4>",sep = " ")})
+  output$header_6 <- renderText({paste("<h4><b>¡Es importante dimensionar la maginitud de estas elecciones!</b></h4>",sep = " ")})
   ###############################################
   ###############################################
   #Tweets
